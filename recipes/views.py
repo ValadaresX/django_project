0883039205858e74d3b67e0c05012b45bbcccfb7
ValadaresX <88790254+ValadaresX@users.noreply.github.com>
2022-07-18 11:00:28 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'global/home.html', context='name': Luiz Otávio)
-
-
-def contato(request):
-    return HttpResponse('contato 1')
-
-
-def sobre(request):
-    return HttpResponse('sobre 1')
+    return render(request, 'recipes/home.html', context={'name': 'Luiz Otávio'
+                                                         })
