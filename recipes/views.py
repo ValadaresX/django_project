5ba1d374f7fb61f9864a24cc253dcf5f66c202f8
ValadaresX <88790254+ValadaresX@users.noreply.github.com>
2022-07-18 12:05:18 -0300

    Peticionando código

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,5 @@
-
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Luiz Otávio'
-                                                         })
+    return render(request, 'recipes/pages/home.html', context={'name': 'Luiz Otávio'})
